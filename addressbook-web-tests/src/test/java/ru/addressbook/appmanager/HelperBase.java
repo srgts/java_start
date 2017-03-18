7b8cd74d57e2b20ed9c3891c2966b787f269cbab
srgts <srgts1@mail.ru>
2017-03-18 23:44:16 +0300

    рефакторинг3

---
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,34 @@
+package ru.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Админ on 18.03.2017.
+ */
+public class HelperBase {
+    protected FirefoxDriver wd;
+
+    public HelperBase(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+    public boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/NavigationHelper.java
@@ -6,14 +6,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Админ on 18.03.2017.
  */
-public class NavigationHelper {
-    final FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
     public NavigationHelper(FirefoxDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void gotoGroupPage() {
-        wd.findElement(By.linkText("groups")).click();
+        click(By.linkText("groups"));
     }
 }
