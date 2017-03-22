ab86393e19a5542716f2a3dfc2123f77dd9e0530
srgts <srgts1@mail.ru>
2017-03-22 16:31:19 +0300

    Запуск тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,9 @@
 package ru.addressbook.appmanager;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -8,15 +11,26 @@ import java.util.concurrent.TimeUnit;
  * Created by Админ on 18.03.2017.
  */
 public class ApplicationManager {
-    FirefoxDriver wd;
+    WebDriver wd;
 
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
 
-    public void init() {
-        wd = new FirefoxDriver();
+    public void init() {                        ;
+        if(browser == BrowserType.FIREFOX){
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        }else if (browser == BrowserType.IE){
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         sessionHelper = new SessionHelper(wd);
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
@@ -3,13 +3,14 @@ package ru.addressbook.tests;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.addressbook.appmanager.ApplicationManager;
+import ru.addressbook.appmanager.BrowserType;
 
 /**
  * Created by Админ on 18.03.2017.
  */
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeMethod
     public void setUp() throws Exception {
