54df0aefc927d24f12b64def27ae2afcea87e7b8
srgts <srgts1@mail.ru>
2017-03-24 17:26:22 +0300

    Реализована проверка и обеспечение предусловия

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package ru.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
@@ -9,6 +10,9 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if(!app.getGroupHelper().isThereAGroup()){
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
