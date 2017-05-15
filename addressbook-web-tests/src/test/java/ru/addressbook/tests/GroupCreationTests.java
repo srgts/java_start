2ab54f176c82e3a6a3ae7aefe5b3676f62033d84
srgts <srgts1@mail.ru>
2017-05-15 22:35:01 +0300

    протоколирование

--- addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/GroupCreationTests.java
@@ -3,13 +3,17 @@ package ru.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.addressbook.model.GroupData;
 import ru.addressbook.model.Groups;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,6 +22,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
         try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")))){
--- addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package ru.addressbook.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.addressbook.appmanager.ApplicationManager;
 import ru.addressbook.appmanager.BrowserType;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by Админ on 18.03.2017.
  */
 public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
@@ -17,9 +26,19 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite (alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestStart (Method m, Object[] p){
+        logger.info("Start" + m.getName() + " with parameters" + Arrays.asList(p));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop (Method m){
+        logger.info("Stop" + m.getName());
     }
 
 }
