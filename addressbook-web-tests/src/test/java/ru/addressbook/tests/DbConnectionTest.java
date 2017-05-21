d8738bf8e759b293120f2df0d65d5d36de3a52d5
srgts <srgts1@mail.ru>
2017-05-21 13:37:19 +0300

    драйвер базы данных

---
+++ addressbook-web-tests/src/test/java/ru/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,41 @@
+package ru.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.addressbook.model.GroupData;
+import ru.addressbook.model.Groups;
+
+import java.sql.*;
+
+/**
+ * Created by Админ on 20.05.2017.
+ */
+public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection (){
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password="+"serverTimezone=UTC");
+            fussa?useLegacyDatetimeCode=false&amp;serverTimezone=UTC
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("SELECT group_id, group_name, group_header, group_footer FROM group_list");
+            Groups groups = new Groups();
+            while(rs.next()){
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            System.out.println(groups);
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
