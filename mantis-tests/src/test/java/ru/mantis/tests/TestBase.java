c2f377d09f23e8c8617a690ba6933d4067a1a788
srgts <srgts1@mail.ru>
2017-07-08 16:56:39 +0300

    Передача файлов на удаленную машину по протоколу FTP

--- mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.BeforeSuite;
 import ru.mantis.appmanager.ApplicationManager;
 import ru.mantis.appmanager.BrowserType;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Created by Админ on 18.03.2017.
  */
@@ -16,10 +19,12 @@ public class TestBase {
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
+        app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.back");
     }
 
     @AfterSuite (alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.back", "config_inc.php");
         app.stop();
     }
 
