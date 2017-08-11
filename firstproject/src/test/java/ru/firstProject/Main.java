3f6f43486851df07bd341f65ad40b9754c81c3a6
srgts <srgts1@mail.ru>
2017-08-11 07:45:25 +0300

    задание-в.1

---
+++ firstproject/src/test/java/ru/firstProject/Main.java
@@ -0,0 +1,91 @@
+package ru.firstProject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public Main(){
+        start();
+    }
+
+    protected final int min = -273;
+    protected final int max = 5526;
+    protected int res = startResultVal(min, max);
+
+
+    public int getRes() {
+        return res;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+        int[] numArr = main.getNumArr();
+        int res = main.getRes();
+
+        for (int i = 0; i < numArr.length; i++) {
+            int x = numArr[i];
+            if (main.isCorrectNum(x)) {
+                if (Math.abs(x) < Math.abs(res) || Math.abs(x) == Math.abs(res) && x > res) {
+                    res = x;
+                }
+            } else {
+                main.printError();
+                res = 0;
+                System.out.println("Число, ближайшее к нулю = " + res);
+            }
+        }
+        System.out.println("Число, ближайшее к нулю = " + res);
+    }
+
+
+    protected boolean isCorrectNum(int val) {
+        if (val > max || val < min) {
+            return false;
+        }
+        return true;
+    }
+
+    protected void printError() {
+        System.out.println("Введены некорректные данные");
+    }
+
+    private int startResultVal(int a, int b) {
+        if (Math.abs(a) > Math.abs(b)) {
+            return a;
+        } else return b;
+    }
+
+    public int[] getNumArr() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numArr[];
+        try {
+            String input = br.readLine();
+            String strArr[] = input.split(" ");
+            numArr = new int[strArr.length];
+
+            for (int i = 0; i < strArr.length; i++) {
+                numArr[i] = Integer.parseInt(strArr[i]);
+            }
+            return numArr;
+        } catch (Exception e) {
+            printError();
+            res = 0;
+            System.out.println("Число, ближайшее к нулю = " + res);
+        }
+        return null;
+    }
+    public void start(){
+        System.out.println("Введите целые числа от " + getMin() + " до " + getMax() + " через пробел:");
+    }
+}
